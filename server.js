6ece2e6f83d00d3f81bb73a75a99effa6ec50b76
Mark <markcfleming1990@gmail.com>
2022-02-09 16:49:20 -0600

    added html

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
 
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+
   // parse incoming string or array data
   app.use(express.urlencoded({ extended: true }));
   // parse incoming JSON data
   app.use(express.json());
+  app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -105,6 +108,21 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('./zookeepr', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 
 
