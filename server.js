9e4d04749107f302db35f1fb19b29fd6547d0d27
Mark <markcfleming1990@gmail.com>
2022-02-21 08:34:32 -0500

    worked on modulaized code

--- server.js
+++ server.js
@@ -1,82 +1,18 @@
-const express = require('express');
-const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
+const { animals } = require('./data/animals');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
   // parse incoming string or array data
   app.use(express.urlencoded({ extended: true }));
   // parse incoming JSON data
   app.use(express.json());
-  app.use(express.static('public'));
-
-function filterByQuery(query, animalsArray) {
-  let personalityTraitsArray = [];
-  let filteredResults = animalsArray;
-  if (query.personalityTraits) {
-    if (typeof query.personalityTraits === 'string') {
-      personalityTraitsArray = [query.personalityTraits];
-    } else {
-      personalityTraitsArray = query.personalityTraits;
-    }
-    personalityTraitsArray.forEach(trait => {
-      filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
-      );
-    });
-  }
-  if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-  }
-  if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
-  }
-  if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
-  }
-  return filteredResults;
-}
-
-function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
-  return result;
-}
-
-function createNewAnimal(body, animalsArray) {
-  const animal = body;
-  //functions main code will go here
-animalsArray.push(animal);
-fs.writeFileSync(
-  //__dirname is the root that the code was executed
-  //path.join() to join the dir name with the path to animals.json
-  path.join(__dirname, './data/animals.json'),
-  //takes the above data and saves as a JSON
-  //null and 2 are formating values Null says not to edit exisiting data
-  //2 creates white space between values to make it more readable
-  JSON.stringify({animals: animalsArray}, null, 2)
-);
-  //return finished code to post route for response
-  return body;
-}
-
-function validateAnimal(animal) {
-  if (!animal.name || typeof animal.name !== 'string') {
-    return false;
-  }
-  if (!animal.species || typeof animal.species !== 'string') {
-    return false;
-  }
-  if (!animal.diet || typeof animal.diet !== 'string') {
-    return false;
-  }
-  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-    return false;
-  }
-  return true;
-}
+  
 
 app.get('/api/animals', (req, res) => {
   let results = animals;
@@ -108,7 +44,7 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
-app.get('./zookeepr', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
@@ -123,8 +59,6 @@ app.get('/zookeepers', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
